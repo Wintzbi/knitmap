fa9354793f7bc0394404b43fe338deb0ecb1944d
CharlilouM <charlilou.maes@gmail.com>
2025-05-07 16:20:59 +0200

    modif menu

--- app/src/main/java/com/example/myapplication/MenuWithDropdown.kt
+++ app/src/main/java/com/example/myapplication/MenuWithDropdown.kt
@@ -15,24 +15,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.zIndex
-import com.example.myapplication.discovery.DiscoveryListActivity
-import com.example.myapplication.friend.FriendListActivity
-import com.example.myapplication.group.GroupListActivity
-import com.example.myapplication.travel.TravelListActivity
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MenuWithDropdown() {
     val context = LocalContext.current
     var expanded by remember { mutableStateOf(false) }
-    var menuSize by remember { mutableStateOf(IntSize.Zero) }
+
+    // Couleurs personnalisées
+    val textColor = Color(0xFF154043)     // Couleur du texte et des séparateurs
+    val backgroundColor = Color(0xFFFFFBED) // Couleur de fond du menu (beige très clair)
+    val borderColor = Color(0xFFE7191F)    // Couleur de la bordure (rouge)
 
     val enterTransition = slideInVertically(initialOffsetY = { -40 }) + fadeIn(initialAlpha = 0.3f)
     val exitTransition = slideOutHorizontally(targetOffsetX = { -200 }) + fadeOut(targetAlpha = 0.3f)
@@ -55,104 +50,72 @@ fun MenuWithDropdown() {
         ) {
             Box(
                 modifier = Modifier
-                    .padding(start = 16.dp, top = 8.dp)
+                    .padding(start = 16.dp, top = 80.dp)
                     .wrapContentSize()
             ) {
-                // Cadre autour
-                Box(
-                    modifier = Modifier
-                        .onGloballyPositioned { coordinates ->
-                            menuSize = coordinates.size
-                        }
-                        .size(
-                            width = with(LocalDensity.current) { menuSize.width.toDp() },
-                            height = with(LocalDensity.current) { menuSize.height.toDp() }
-                        )
-                        .border(
-                            width = 4.dp,
-                            color = Color.Transparent,
-                            shape = RoundedCornerShape(0.dp)
-                        )
-                ) {
-                    // 4 lignes superposées via Box
-                    Box(modifier = Modifier
-                        .matchParentSize()
-                        .zIndex(1f)) {
-                        // Ligne en haut
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .height(4.dp)
-                                .background(Color(0xFFD1EAD3))
-                                .align(Alignment.TopCenter)
-                        )
-                        // Ligne en bas
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .height(4.dp)
-                                .background(Color(0xFFFFE6A7))
-                                .align(Alignment.BottomCenter)
-                        )
-                        // Ligne gauche
-                        Box(
-                            Modifier
-                                .fillMaxHeight()
-                                .width(4.dp)
-                                .background(Color(0xFFEB6B63))
-                                .align(Alignment.CenterStart)
-                        )
-                        // Ligne droite
-                        Box(
-                            Modifier
-                                .fillMaxHeight()
-                                .width(4.dp)
-                                .background(Color(0xFFADD3F5))
-                                .align(Alignment.CenterEnd)
-                        )
-                    }
-                }
-
-                // Menu
                 DropdownMenu(
                     expanded = expanded,
                     onDismissRequest = { expanded = false },
                     modifier = Modifier
-                        .background(Color(0xFFFFFCED))
-                        .clip(RoundedCornerShape(8.dp))
-                        .zIndex(2f) // au-dessus des lignes
-                ) {
-                    DropdownMenuItem(text = { Text("Profil") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, ProfilActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Map") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, MapActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Voyages") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, TravelListActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Amis") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, FriendListActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Groupe") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, GroupListActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Découverte") }, onClick = {
-                        expanded = false
-                        context.startActivity(Intent(context, DiscoveryListActivity::class.java))
-                    })
-                    DropdownMenuItem(text = { Text("Déconnexion") }, onClick = {
-                        expanded = false
-                        FirebaseAuth.getInstance().signOut()
-                        val logoutIntent = Intent(context, MainActivity::class.java)
-                        logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        context.startActivity(logoutIntent)
-                    })
+                        .background(color = backgroundColor,
+                            shape = RoundedCornerShape(22.dp),
+
+                        )
+                        .border(
+                            width = 1.5.dp,
+                            color = borderColor,
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                        .clip(RoundedCornerShape(30.dp))
+                )
+                {
+                    // Premier élément
+                    DropdownMenuItem(
+                        text = { Text("Profil", color = textColor) },
+                        onClick = {
+                            expanded = false
+                            context.startActivity(Intent(context, ProfilActivity::class.java))
+                        }
+                    )
+
+                    // Séparateur
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                            .height(1.dp)
+                            .background(textColor)
+                    )
+
+                    // Deuxième élément
+                    DropdownMenuItem(
+                        text = { Text("Map", color = textColor) },
+                        onClick = {
+                            expanded = false
+                            context.startActivity(Intent(context, MapActivity::class.java))
+                        }
+                    )
+
+                    // Séparateur
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                            .height(1.dp)
+                            .background(textColor)
+                    )
+
+                    // Troisième élément
+                    DropdownMenuItem(
+                        text = { Text("Déconnexion", color = textColor) },
+                        onClick = {
+                            expanded = false
+                            FirebaseAuth.getInstance().signOut()
+                            val logoutIntent = Intent(context, MainActivity::class.java)
+                            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            context.startActivity(logoutIntent)
+                        }
+                    )
                 }
             }
         }
